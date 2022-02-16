b4e92dc1c09d9630b9a74c12e21e1e0c8623eed6
teo-core <tsanchez@corenetworks.es>
2022-02-16 10:34:48 +0100

    validar archivo y contar lineas

--- pruebas/test_wc.py
+++ pruebas/test_wc.py
@@ -2,6 +2,40 @@ import unittest
 import wc
 
 class Testwc(unittest.TestCase):
+    maxDiff = None
     def test_existe_wc(self):
         w = wc.Wc()
         self.assertIsNotNone(w)
+
+    def test_validar_archivo_texto_es_correcto(self):
+        w = wc.Wc('/home/teo/proyectos/wc/uno.html')
+        resp = w._Wc__validar_archivo()
+        self.assertEqual(resp, True)
+
+    def test_validar_archivo_texto_es_incorrecto(self):
+        w = wc.Wc('/home/teo/proyectos/wc/bolas_chicle.jpg')
+        resp = w._Wc__validar_archivo()
+        self.assertEqual(resp, False)        
+
+    def test_validar_archivo_inexistente_es_incorrecto(self):
+        w = wc.Wc('/home/teo/proyectos/wc/bolas_chicle.jpg')
+        resp = w._Wc__validar_archivo()
+        self.assertEqual(resp, False)  
+
+    def test_abrir_archivo_correcto(self):
+        html = """Hola\ncaracola sdlkrg ñsldkj m"""
+        w = wc.Wc('/home/teo/proyectos/wc/uno.html')
+        w.abrir_archivo()
+        self.assertEqual(w._Wc__contenido,html )
+    
+    def test_abrir_archivo_inexistente_da_error(self):
+        w = wc.Wc('/home/teo/ggg')
+        with self.assertRaises(Exception):
+            resp = w.abrir_archivo()
+              
+    def test_contar_lineas_correcto(self):
+        html = """Hola\ncaracola sdlkrg ñsldkj m"""
+        w = wc.Wc('/home/teo/proyectos/wc/uno.html')
+        w.abrir_archivo()
+        w.contar_lineas()
+        self.assertEqual(w._Wc__cnt_lineas,2)
--- wc.py
+++ wc.py
@@ -1,18 +1,41 @@
-# import mimetypes
-
-# c=open('/home/teo/proyectos/wc/README.md')
-# mime = mimetypes.guess_type('/home/teo/proyectos/wc/project1.ico')
-# print(mime)
+import mimetypes
 
 class Wc():
-    def __init__(self) -> None:
+    def __init__(self,archivo='') -> None:
+        self.__archivo = archivo
         self.__cnt_palabras = 0
         self.__cnt_lineas = 0
         self.__ocurrencias = {}
+        self.__contenido = ''
     
     @property
     def numero_palabras(self):
-        pass
+        return self.__cnt_palabras
     
+
+    def __validar_archivo(self):
+        mime = mimetypes.guess_type(self.__archivo)
+        try:
+            resp = mime[0].split('/')[0] == 'text'
+            return resp
+        except AttributeError:
+            raise Exception('Archivo inexistente o inválido')
+
+
+    def abrir_archivo(self):
+        try:
+            if self.__validar_archivo():
+                with open(self.__archivo,'r') as manejador:
+                    texto = manejador.read()
+                    if texto != '':
+                        self.__contenido = texto
+        except Exception as e:
+            raise e
+
+    def contar_lineas(self):
+        self.__cnt_lineas = len(self.__contenido.split('\n'))
+
+    
+
 
         
